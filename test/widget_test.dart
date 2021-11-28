0fcc98bc41aef174a9fbb037811c9df8830baddb
SharmaPrateek196 <sharmaprateek196@gmail.com>
2021-11-28 09:51:21 +0530

    remove: unneccesary code from main.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cred/ui/main.dart';
+import 'package:cred/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
