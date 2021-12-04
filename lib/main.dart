93de3795e8022416b320237799ff596da418432d
SharmaPrateek196 <sharmaprateek196@gmail.com>
2021-12-05 01:07:38 +0530

    bottom nav icons alignment

--- lib/main.dart
+++ lib/main.dart
@@ -54,20 +54,26 @@ class MyHomePage extends StatelessWidget {
                 decoration: BoxDecoration(
                   color: HexColor.fromHex(ExtraDarkBackground),
                 ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    RadiantGradientMask(
-                      child: IconButton(icon: Icon(Icons.home, color: Colors.white,), onPressed: () {}),
-                      gradient: pinkRadialGradientCenterBottomRight,
-                    ),
-                    IconButton(icon: Icon(Icons.home), onPressed: () {}),
-                    IconButton(icon: Icon(Icons.home), onPressed: () {}),
-                    IconButton(icon: Icon(Icons.home), onPressed: () {}),
-                    IconButton(icon: Icon(Icons.home), onPressed: () {}),
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      RadiantGradientMask(
+                        child: IconButton(icon: Icon(Icons.home, size: 35, color: Colors.white,), onPressed: () {}),
+                        gradient: pinkRadialGradientCenterBottomRight,
+                      ),
+                      IconButton(icon: Icon(Icons.home, size: 35,), onPressed: () {}),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 28.0, right: 34.0),
+                        child: IconButton(icon: Icon(Icons.circle, size: 70,), onPressed: () {}),
+                      ),
+                      IconButton(icon: Icon(Icons.home, size: 35,), onPressed: () {}),
+                      IconButton(icon: Icon(Icons.home, size: 35,), onPressed: () {}),
+                    ],
+                  ),
                 ),
               ),
               CustomPaint(
