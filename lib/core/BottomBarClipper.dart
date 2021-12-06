a01dedc6c466f07d656de20d7bafedd7a1727d15
SharmaPrateek196 <sharmaprateek196@gmail.com>
2021-12-07 01:20:00 +0530

    udpate: bottom bar border path

--- lib/core/BottomBarClipper.dart
+++ lib/core/BottomBarClipper.dart
@@ -8,10 +8,10 @@ class BottomBarClipper extends CustomClipper<Path> {
   Path getClip(Size size) {
     Path path = Path();
     path.moveTo(0, size.height*0.3);
-    path.lineTo(size.width*0.30, size.height*0.3);
+    path.lineTo(size.width*0.26, size.height*0.3);
     path.quadraticBezierTo(size.width*0.33, size.height*0.3, size.width*0.36, size.height*0.21);
     path.quadraticBezierTo(size.width*0.5, -size.height*0.15, size.width*0.64, size.height*0.21);
-    path.quadraticBezierTo(size.width*0.67, size.height*0.3, size.width*0.7, size.height*0.3);
+    path.quadraticBezierTo(size.width*0.67, size.height*0.3, size.width*0.74, size.height*0.3);
     path.lineTo(size.width, size.height*0.3);
     path.lineTo(size.width, size.height);
     path.lineTo(0, size.height);
@@ -28,12 +28,12 @@ class BottomBarBorderPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     Path borderPath = Path();
-    borderPath.moveTo(size.width*0.15, size.height*0.3);
-    borderPath.lineTo(size.width*0.30, size.height*0.3);
+    borderPath.moveTo(size.width*0.1, size.height*0.29);
+    borderPath.lineTo(size.width*0.26, size.height*0.3);
     borderPath.quadraticBezierTo(size.width*0.33, size.height*0.3, size.width*0.36, size.height*0.21);
     borderPath.quadraticBezierTo(size.width*0.5, -size.height*0.15, size.width*0.64, size.height*0.21);
-    borderPath.quadraticBezierTo(size.width*0.67, size.height*0.3, size.width*0.7, size.height*0.3);
-    borderPath.lineTo(size.width*0.85, size.height*0.3);
+    borderPath.quadraticBezierTo(size.width*0.67, size.height*0.3, size.width*0.74, size.height*0.3);
+    borderPath.lineTo(size.width*0.9, size.height*0.29);
 
     Paint brush = Paint()
     ..color = HexColor.fromHex(Pink500)
