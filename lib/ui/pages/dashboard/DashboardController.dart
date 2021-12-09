8e959448aa7e10f443a80c910fa65718e61468ee
SharmaPrateek196 <sharmaprateek196@gmail.com>
2021-12-09 23:57:52 +0530

    redundant code removal

--- lib/ui/pages/dashboard/DashboardController.dart
+++ lib/ui/pages/dashboard/DashboardController.dart
@@ -1,9 +1,7 @@
 import 'package:get/get.dart';
-import 'package:logger/logger.dart';
 
 class DashboardController extends GetxController {
   var currentTabIndex = 0.obs;
-  var logger = Logger();
 
   void changeTabIndex(int newTabIndex) {
     this.currentTabIndex.value = newTabIndex;
